1f31fa654a6dce261a3570fd675d2b8e9eae165e
Fady Ramzy <fady.h.ramzy@gmail.com>
2021-07-03 14:46:37 +0200

    Project initiation

---
+++ Shared/Tempo_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tempo_TaskApp.swift
+//  Shared
+//
+//  Created by Fady Ramzy on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tempo_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
