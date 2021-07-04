1de7d363d623fa6caa84f3b385966688899315b3
Fady Ramzy <fady.h.ramzy@gmail.com>
2021-07-04 12:01:30 +0200

    - Added news model

---
+++ iOS/News/Model/News.swift
@@ -0,0 +1,21 @@
+//
+//  News.swift
+//  Tempo-Task (iOS)
+//
+//  Created by Fady Ramzy on 04/07/2021.
+//
+
+import Foundation
+
+struct News {
+    
+    // MARK: - Properties
+    
+    var title: String?
+    var description: String?
+    var author: String?
+    var source: String?
+    var Content: String?
+    var date: String?
+    var image: String?
+}
